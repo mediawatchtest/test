32359779631765db52440828fd47116998f2083b
ruslan <Ruslan@il-ruslan.interactive-ltd.com>
2014-10-26 15:57:00 +0400

    testing

--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test.Properties")]
+[assembly: AssemblyTitle("test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test.Properties")]
+[assembly: AssemblyProduct("test")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,7 +20,7 @@ using System.Resources;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("081d08a3-b2a3-481b-b130-0c85f545ae67")]
+[assembly: Guid("688636c4-224c-4c2c-bf2a-a46eaf39baf8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,7 +29,7 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
