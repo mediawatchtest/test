a7f0c475995d08068dbd32dfaafe15ec431c4b1c
ruslan <Ruslan@il-ruslan.interactive-ltd.com>
2014-11-02 14:13:17 +0400

    tests passes

--- unit.tests/tests.cs
+++ unit.tests/tests.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
-using Xunit;
-
-
-
+    using Xunit;
 
 
     /* some comments*/
-
-namespace unit.tests
-{
-    public class tests
+    namespace unit.tests
     {
-            
-        [Fact]
-        public void test(){
+        public class tests
+        {
+                
+            [Fact]
+            public void test(){
 
-            Assert.True(true);
-            Assert.True(true);
-            Assert.True(true);
-            Assert.True(false);
+                Assert.True(true);
+                Assert.True(true);
+                Assert.True(true);
+                Assert.True(true);
 
+            }
         }
-    }
 
-    
-}
+        
+    }
